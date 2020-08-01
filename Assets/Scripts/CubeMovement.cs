22c953a01d615e26b0e63b9d2a46a34fb1a5e195
boofala <boofala@cox.net>
2020-08-01 11:18:42 -0700

    init by sarah

---
+++ Assets/Scripts/CubeMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+    //[SerializeField]
+    public float cube1Speed = 1;
+    // Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 cubeMovement = new Vector3(horizontalInput, verticalInput);
+
+        transform.position += cubeMovement * Time.deltaTime * cube1Speed;
+    }
+}
